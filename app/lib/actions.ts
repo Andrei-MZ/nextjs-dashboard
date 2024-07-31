771f10afec305f773492a9c90a9df5b31abe0d34
andrei <andreimendes8@gmail.com>
2024-07-31 08:28:48 -0300

    feat: create new dates

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -18,4 +18,6 @@ export async function createInvoice(formData: FormData) {
       amount: formData.get('amount'),
       status: formData.get('status'),
     });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
   }
