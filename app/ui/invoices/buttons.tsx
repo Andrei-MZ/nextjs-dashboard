0dee8c4507fdb3918a6c06a6b0beafdaa05f0eb8
andrei <andreimendes8@gmail.com>
2024-08-01 09:44:42 -0300

    new script button

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -2,9 +2,6 @@ import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { deleteInvoice } from '@/app/lib/actions';
 
-export function DeleteInvoice({ id }: { id: string }) {
-const deleteInvoiceWithId = deleteInvoice.bind(null, id);
-
 export function CreateInvoice() {
   return (
     <Link
@@ -20,7 +17,7 @@ export function CreateInvoice() {
 export function UpdateInvoice({ id }: { id: string }) {
   return (
     <Link
-    href={`/dashboard/invoices/${id}/edit`}
+      href={`/dashboard/invoices/${id}/edit`}
       className="rounded-md border p-2 hover:bg-gray-100"
     >
       <PencilIcon className="w-5" />
@@ -29,6 +26,8 @@ export function UpdateInvoice({ id }: { id: string }) {
 }
 
 export function DeleteInvoice({ id }: { id: string }) {
+  const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+
   return (
     <form action={deleteInvoiceWithId}>
       <button className="rounded-md border p-2 hover:bg-gray-100">
